742b6869ce6db306adbdc53b6d2769e20ce03519
帅的一塌糊涂 <test@123.com>
2019-01-16 14:17:16 +0800

    【20190116】第2次提版

--- testDir/PrintInfo.py
+++ testDir/PrintInfo.py
@@ -3,5 +3,15 @@
 def printI():
     print(123)
 
+
+def suminfo():
+    sum = 0
+    for i in range(100):
+        sum += i
+    return i
+
 if __name__ == "__main__":
     printI()
+    
+    t = suminfo()
+    print(t)
