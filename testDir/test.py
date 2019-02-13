78946520db864eaa6fd32f3cca86d51df3acead5
帅的一塌糊涂 <test@123.com>
2019-02-13 14:40:47 +0800

    20190213黄不爽体版

--- testDir/test.py
+++ testDir/test.py
@@ -1,7 +1,7 @@
 import time
 
 def printinfo():
-    while  True:
+    #while  True:
         print("你个帅比")
         time.sleep(1)
 
@@ -20,3 +20,29 @@ print("root user")
 
 print(2222222222222222222222222222222222222222222)
 print(3333333333333333333333333333333333333333333)
+
+
+class A:
+
+    def A1(self):
+        print('this is A1')
+        return 0
+
+    def A2(self):
+        print('刘二叉')
+
+class B(A):
+    def B1(self):
+        print('Class B')
+        self.A2()
+
+    def A1(self):
+        print('this is B2')
+
+if __name__ == "__main__":
+    b = B()
+    b.B1()
+    b.A1()
+
+    a = A()
+    a.A2()
